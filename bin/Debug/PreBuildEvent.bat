ac2a5c98aa43cacc7c286d57f0fc8e9cddae7ae7
rancho@mwwb.net <rancho@mwwb.net@0e435444-ff55-11dd-b507-8b4ecd5210be>
2009-02-20 17:59:36 +0000

    do not need

--- bin/Debug/PreBuildEvent.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-DATE 09/21/2008
-if errorlevel 1 goto CSharpReportError
-goto CSharpEnd
-:CSharpReportError
-echo Project error: A tool returned an error code from the build event
-exit 1
-:CSharpEnd
--- bin/Release/PostBuildEvent.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-copy C:\Bob\Round_2\Dispersal_Sim\bin\Release\Dispersal_Sim.exe W:\Zollner-lab\Dispersal_Sim.exe 
-if errorlevel 1 goto CSharpReportError
-goto CSharpEnd
-:CSharpReportError
-echo Project error: A tool returned an error code from the build event
-exit 1
-:CSharpEnd
