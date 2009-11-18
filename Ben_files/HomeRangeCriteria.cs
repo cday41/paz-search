4908b0e93aaaf12fa3747d0bc85bb7e6e604dadb
bpauli@purdue.edu <bpauli@purdue.edu@0e435444-ff55-11dd-b507-8b4ecd5210be>
2009-11-18 20:42:05 +0000

    Version where Ben changed mortality calculation and changed offspring mean and SD from integers to doubles

---
+++ Ben_files/HomeRangeCriteria.cs
@@ -0,0 +1,76 @@
+using System;
+
+namespace SEARCH
+{
+	/// <summary>
+	/// contains the criteria for calculating the home range area
+	/// </summary>
+	public class HomeRangeCriteria : IHomeRangeCriteria
+	{
+		#region Public Members (2) 
+
+		#region Constructors (2) 
+
+      public HomeRangeCriteria (double inArea,double inDM, double inDSD,double inDW)
+      {
+         mArea=inArea;
+         mDistanceMean=inDM;
+         mDistanceSD=inDSD;
+         mDistanceWeight=inDW;
+      }
+
+		public HomeRangeCriteria()
+      {
+         mArea=0;
+         mDistanceMean=0;
+         mDistanceSD=0;
+         mDistanceWeight=0;
+      }
+
+		#endregion Constructors 
+
+		#endregion Public Members 
+
+		#region Non-Public Members (4) 
+
+		#region Fields (4) 
+
+      private double mArea;
+      private double mDistanceMean;
+      private double mDistanceSD;
+      private double mDistanceWeight;
+
+		#endregion Fields 
+
+		#endregion Non-Public Members 
+
+
+      #region IHomeRangeCriteria Members
+      public double Area
+		{
+			get { return mArea; }
+			set  { mArea = value; }
+		}
+
+      public double DistanceMean
+		{
+			get { return mDistanceMean; }
+			set  { mDistanceMean = value; }
+		}
+
+      public double DistanceSD
+		{
+			get { return mDistanceSD; }
+			set  { mDistanceSD = value; }
+		}
+
+      public double DistanceWeight
+		{
+			get { return mDistanceWeight; }
+			set  { mDistanceWeight = value; }
+		}
+
+     
+      #endregion
+   }
+}
---
+++ Ben_files/RiskyForage.cs
@@ -0,0 +1,18 @@
+namespace SEARCH
+{
+   public class RiskyForageModifier : Modifier
+   {
+		#region Public Members (1) 
+
+		#region Constructors (1) 
+
+      public RiskyForageModifier()
+      {
+         base.Name = "RiskyForageModifier";
+      }
+
+		#endregion Constructors 
+
+		#endregion Public Members 
+   }
+}
