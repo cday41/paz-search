c1a3bc3ff2412fd6cab679e8deec6fa64ec7724d
rancho@mwwb.net <rancho@mwwb.net@0e435444-ff55-11dd-b507-8b4ecd5210be>
2012-11-18 01:17:56 +0000

    Initial Creation

---
+++ Serialize Example/Serialization/Serialization/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+   [Serializable()]
+  public class Animal
+   {
+		#region Fields (3) 
+
+      int age;
+      bool alive;
+      string sex;
+
+		#endregion Fields 
+
+		#region Constructors (1) 
+
+      public Animal()
+      {
+         alive = true;
+      }
+      public Animal(int inAge, string inSex):this()
+      {
+         age = inAge;
+         sex = inSex;
+   
+
+      }
+
+		#endregion Constructors 
+
+      public override string ToString()
+      {
+         return "My age is " + age.ToString() + " sex  is " + sex + " alive is " + alive.ToString();
+      }
+
+		#region Properties (3) 
+
+      public int Age
+      {
+         get { return age; }
+         set { age = value; }
+      }
+
+      public bool Alive
+      {
+         get { return alive; }
+         set { alive = value; }
+      }
+
+      public string Sex
+      {
+         get { return sex; }
+         set { sex = value; }
+      }
+
+		#endregion Properties 
+   }
+}
---
+++ Serialize Example/Serialization/Serialization/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+   class Program
+   {
+      #region Methods (2)
+
+      // Private Methods (2) 
+
+      static void Main(string[] args)
+      {
+         string fileName = "animal.xml";
+         List<Animal> myAnimals = null;
+         Console.Write("Num args " + args.Count().ToString());
+         Console.ReadKey();
+         if (args.Count() == 0)
+            myAnimals = MakeBrandNewAnimals();
+         else
+            myAnimals = SerializeHelper.DeserializeFromFile(fileName, new List<Animal>()) as List<Animal>;
+
+
+         foreach (Animal a in myAnimals)
+         {
+
+            Console.WriteLine(a.ToString());
+         }
+
+         Console.WriteLine("Press a key to continue");
+         Console.ReadKey();
+
+         int i = 0;
+         foreach (Animal a in myAnimals)
+         {
+            if (i % 2 == 0)
+            {
+               a.Sex = "female";
+
+            }
+            if (i % 5 == 0)
+            {
+               a.Alive = false;
+            }
+            Console.WriteLine(a.ToString());
+            i++;
+
+         }
+         Console.WriteLine("Press a key to continue");
+         Console.ReadKey();
+
+         SerializeHelper.SerializeObjectToFile(fileName, myAnimals);
+      }
+
+      static List<Animal> MakeBrandNewAnimals()
+      {
+         List<Animal> returnValue = new List<Animal>();
+         for (int i = 0; i < 10; i++)
+         {
+            Animal a = new Animal(i, "male");
+            returnValue.Add(a);
+         }
+         return returnValue;
+      }
+
+      #endregion Methods
+   }
+}
+
